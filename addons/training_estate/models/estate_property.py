ff58961303d4718b6d607f0d672fd42705239079
Yonattan <yonattan_hope@yahoo.com>
2023-05-31 16:39:13 +0700

    adding some field and default value

--- addons/training_estate/models/estate_property.py
+++ addons/training_estate/models/estate_property.py
@@ -5,20 +5,26 @@ class EstateModel(models.Model):
     # sampai baris sini sudah cukup buat bikin tabel
     _description = "Estate Property models"
     name = fields.Char('Estate Name', required=True, default="Unknown")
-    last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
-    description = fields.Text('Estate Description')
+    description = fields.Text('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availability Date', copy=False, default=lambda self: fields.Datetime.add(fields.Datetime.now(), months=3))
+    date_availability = fields.Date('Available From', copy=False, default=lambda self: fields.Datetime.add(fields.Datetime.now(), months=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Sell Price', readonly=True, copy=False)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
     bedrooms = fields.Integer('BedRooms', default=2)
-    living_area = fields.Integer('Living Area')
+    living_area = fields.Integer('Living Area (sqm)')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
-    garden_area = fields.Integer('Garden Area')
+    garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(
         string='Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
         help="Orientation is used to show garden orientation"
     )
+    state = fields.Selection(
+        string='Status',
+        selection=[('new', 'New'), ('received', 'Offer Received'), ('accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
+        help="Estate condition"
+    )
+    active = fields.Boolean(default=True)
+    last_seen = fields.Datetime("Last Seen", default=lambda self: fields.Datetime.now())
