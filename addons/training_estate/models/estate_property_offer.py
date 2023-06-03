98a4edb1e92d2cd3a44352f4ade4b8cf6548ce47
Yonattan <yonattan_hope@yahoo.com>
2023-06-03 13:01:53 +0700

    blm selesai dgn chpater 9 inverse

--- addons/training_estate/models/estate_property_offer.py
+++ addons/training_estate/models/estate_property_offer.py
@@ -1,5 +1,4 @@
-from odoo import fields, models
-
+from odoo import fields, models, api
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -13,4 +12,19 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True, string="Partner")
     property_id = fields.Many2one("estate.property", required=True, string="Property")
+    validity = fields.Integer('Validity (days)', compute='_compute_validity', required=True, default="7")
+    date_deadline = fields.Date('Deadline', required=True)
+    # inverse = '_inverse_validity',
+    @api.depends('validity')
+    def _compute_validity(self):
+        for record in self:
+            if record.create_date:
+                date = fields.Datetime.now().date()
+            else:
+                date = record.create_date
+            record.date_deadline = fields.Datetime.add(date, days=record.validity)
 
+    # def _inverse_validity(self):
+    #     for lead in self:
+    #         if lead.partner_id and lead.email_from != lead.partner_id.email:
+    #             lead.partner_id.email = lead.email_from
