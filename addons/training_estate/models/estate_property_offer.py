4c60f84493012a76c500dad0a98956bc86e06b09
Yonattan <34703013+lie16@users.noreply.github.com>
2023-06-04 12:53:26 +0700

    FIX menggunakan datetime

--- addons/training_estate/models/estate_property_offer.py
+++ addons/training_estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
 from odoo import fields, models, api
+from datetime import datetime, timedelta
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -12,19 +13,29 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True, string="Partner")
     property_id = fields.Many2one("estate.property", required=True, string="Property")
-    validity = fields.Integer('Validity (days)', compute='_compute_validity', required=True, default="7")
-    date_deadline = fields.Date('Deadline', required=True)
+    validity = fields.Integer('Validity (days)', required=True, default=7)
+    date_deadline = fields.Date('Deadline', compute='_compute_validity', required=True)
     # inverse = '_inverse_validity',
     @api.depends('validity')
     def _compute_validity(self):
         for record in self:
+            print("validity = %s" % record.validity)
             if record.create_date:
-                date = fields.Datetime.now().date()
+                start_date = record.create_date
             else:
-                date = record.create_date
-            record.date_deadline = fields.Datetime.add(date, days=record.validity)
+                start_date = fields.Datetime.now()
+            if record.validity:
+                duration = timedelta(days=record.validity)
+            else:
+                duration = 0
+            record.date_deadline = start_date + duration
 
     # def _inverse_validity(self):
-    #     for lead in self:
-    #         if lead.partner_id and lead.email_from != lead.partner_id.email:
-    #             lead.partner_id.email = lead.email_from
+    #     for record in self:
+    #         if record.create_date:
+    #             createdate = fields.Datetime.now()
+    #         else:
+    #             createdate = record.create_date
+    #         print('Tanggal date: %s' % createdate)
+    #         record.validity = record.date_deadline - createdate
+    #         print('Validity: %s' % record.validity)
